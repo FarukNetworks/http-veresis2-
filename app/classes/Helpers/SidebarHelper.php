d6943ed6570a4ac5d2d443117c372747a858a64a
matichek <mstrlekar@gmail.com>
2024-05-18 15:57:26 +0200

    Update padding in navbar and sidebar for better spacing and alignment

--- app/classes/Helpers/SidebarHelper.php
+++ app/classes/Helpers/SidebarHelper.php
@@ -11,37 +11,51 @@ class SidebarHelper
 	static function getItems() {
 
 		$items = [
-
-			array('page' =>'narocila','title' => 'Naročila', 'permissions' => 'orders'),
-
-			array('page' =>'odpreme','title' => 'Odpreme', 'permissions' => 'orders'),
-
-			array('page' =>'skatle','title' => 'Škatle', 'permissions' => 'orders'),
-
-			array('permissions' => 'all','dropdown' =>'test', 'dropdownidentity' => 1,'title' => 'Uničene tablice', 
-
-					'childs' => array(
-
-						array('dropdownidentity' => 1, 'dropdownItem' =>'unicene-tablice','dropdownItemTitle' => 'Iskanje uničenih tablic'),
-
-						array('dropdownidentity' => 1, 'dropdownItem' =>'ponarejene-tablice','dropdownItemTitle' => 'Iskanje ponaredkov')
-
-					) 
-
+			array('page' =>'narocila','title' => 'Naročila', 'permissions' => 'orders', 'icon' => ' glyphicon-shopping-cart'),
+			array('page' =>'odpreme','title' => 'Odpreme', 'permissions' => 'orders', 'icon' => ' glyphicon-send'),
+			array('page' =>'skatle','title' => 'Škatle', 'permissions' => 'orders', 'icon' => ' glyphicon-gift'),
+			array(
+				'permissions' => 'all',
+				'dropdown' =>'test',
+				'dropdownidentity' => 1,
+				'title' => 'Uničene tablice',
+				'icon' => ' glyphicon-trash',
+				'childs' => array(
+					array('dropdownidentity' => 1, 'icon' => ' glyphicon-trash', 'dropdownItem' =>'unicene-tablice','dropdownItemTitle' => 'Iskanje uničenih tablic'),
+					array('dropdownidentity' => 1, 'icon' => ' glyphicon-search', 'dropdownItem' =>'ponarejene-tablice','dropdownItemTitle' => 'Iskanje ponaredkov')
+				)
 			),
-
-			array('page' =>'paketi','title' => 'Paketi', 'permissions' => 'orders'),
-
-			array('link' =>'http://kig.si/trgovina/','title' => 'Spletna trgovina', 'permissions' => 'all')/*,
-
-			array('link' =>'http://reklamne-tablice.si/konfigurator/','title' => 'Reklamne tablice', 'permissions' => 'all')*/
-
+			array('page' =>'paketi','title' => 'Paketi', 'permissions' => 'orders', 'icon' => ' glyphicon-briefcase')
+			// array('link' =>'http://kig.si/trgovina/','title' => 'Spletna trgovina', 'permissions' => 'all', 'icon' => 'glyphicon-user')
 		];
 
 		return $items;  
 
 	}
 
+
+// 	case 'Naročila':
+// 		$icon = 'glyphicon-shopping-cart';
+// 		break;
+// 	case 'Odpreme':
+// 		$icon = 'glyphicon-send';
+// 		break;
+// 	case 'Škatle':
+// 		$icon = 'glyphicon-inbox';
+// 		break;
+// 	case 'Uničene tablice':
+// 		$icon = 'glyphicon-fire';
+// 		break;
+// 	case 'Paketi':
+// 		$icon = 'glyphicon-gift';
+// 		break;
+// 	case 'Spletna trgovina':
+// 		$icon = 'glyphicon-globe';
+// 		break;
+// 	default:
+// 		$icon = 'glyphicon-user';
+// 		break;
+// }
 
 
 	static function getSidebarItems($permissions) {
